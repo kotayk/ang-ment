307b1d591049d37904dfc6a87f577d2726113d38
ilia_fedorov <ivfdrv@yandex.ru>
2019-02-13 11:54:07 +0300

    login component logic was changed

--- src/app/common/user-login/user-login.component.ts
+++ src/app/common/user-login/user-login.component.ts
@@ -1,28 +1,20 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.scss']
 })
-export class UserLoginComponent implements OnInit, OnChanges {
+export class UserLoginComponent implements OnInit {
   @Input() userData: any;
   @Output() logoutClick: EventEmitter<any> = new EventEmitter();
-  firstName: string;
-  lastName: string;
   isAuthenticated: boolean;
 
   constructor() { }
 
   ngOnInit() {}
 
-  ngOnChanges(change) {
-    if (change.userData && Object.keys(change.userData.currentValue).length) {
-      this.firstName = change.userData.currentValue.name.first;
-      this.lastName = change.userData.currentValue.name.last;
-      this.isAuthenticated = true;
-    }
-  }
+
 
   onLogoutClick() {
     this.logoutClick.emit();
