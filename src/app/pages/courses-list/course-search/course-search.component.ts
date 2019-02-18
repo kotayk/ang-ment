391f218cdfd4d9136ac1a8b035fb4ccb908119f9
ilia_fedorov <ivfdrv@yandex.ru>
2019-02-18 12:37:07 +0300

    unsubscribe from observable

--- src/app/pages/courses-list/course-search/course-search.component.ts
+++ src/app/pages/courses-list/course-search/course-search.component.ts
@@ -1,18 +1,19 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {fromEvent, Observable, Subject, timer} from 'rxjs';
-import {debounce, filter, map} from 'rxjs/operators';
+import {debounce, filter, map, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-course-search',
   templateUrl: './course-search.component.html',
   styleUrls: ['./course-search.component.scss']
 })
-export class CourseSearchComponent implements OnInit {
+export class CourseSearchComponent implements OnInit, OnDestroy {
   @Output() searchClick: EventEmitter<any> = new EventEmitter();
   @ViewChild('searchInput') searchInput: ElementRef;
   searchQuery: string;
   inputChange: Observable<any>;
+  unsubscribe = new Subject();
 
   constructor(private router: Router) {
   }
@@ -21,10 +22,10 @@ export class CourseSearchComponent implements OnInit {
     this.inputChange = fromEvent(this.searchInput.nativeElement, 'input').pipe(
       map((e: any) => e.target.value),
       filter((val: string) => val.length >= 3 || !val.length),
-      debounce(() => timer(500))
-    );
+      debounce(() => timer(500)),
+      takeUntil(this.unsubscribe)
+  );
     this.inputChange.subscribe((val) => {
-      console.log(val)
       this.searchClick.emit(val);
     });
   }
@@ -35,6 +36,11 @@ export class CourseSearchComponent implements OnInit {
 
   onClickAdd(event: MouseEvent) {
     this.router.navigate(['/courses/new']);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 
