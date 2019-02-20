7777c9b2bfb070bf7bf5e923ad926f3422b8d315
ilia_fedorov <ivfdrv@yandex.ru>
2019-02-20 18:50:35 +0300

    debounce spinner state change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, NavigationStart, Router} from '@angular/router';
 import {SpinnerService} from './common/services/spinner.service';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.isLoading = false;
-    this.spinnerService.requestsCounter.subscribe(counter => {
+    this.spinnerService.requestsCounter.pipe(debounceTime(0)).subscribe(counter => {
       this.isLoading = counter > 0;
     });
 
